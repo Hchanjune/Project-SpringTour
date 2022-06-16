045514fcd7c4ca5b5037addfc25becd75431b3ca
stuicrge <90564523+stuicrge@users.noreply.github.com>
2022-06-16 15:29:07 +0900

    TourChoice1

--- src/main/java/com/team/springtour/controller/tourPackage/TourPackageController.java
+++ src/main/java/com/team/springtour/controller/tourPackage/TourPackageController.java
@@ -1,5 +1,50 @@
 package com.team.springtour.controller.tourPackage;
 
-public class TourPackageController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.team.springtour.domain.tourPackage.TourPackageDto;
+import com.team.springtour.service.tourPackage.TourPackageService;
+
+@Controller
+@RequestMapping("tourPackage")
+public class TourPackageController {
+		
+	
+	@Autowired
+	private TourPackageService service;
+	
+	@GetMapping("TourChoice")
+	public void choicePage(Model model) {
+		
+		List<TourPackageDto> list = service.TourList();
+		System.out.println(list);
+		model.addAttribute("tourList",list);
+	
+	}
+	// getmapping 값이 view의 jsp와 같으면 void를 사용한다
+	@GetMapping("HoneymoonTour")
+	public void honeymoon() {
+			
+	}
+	
+	@GetMapping("GolfTour")
+	public void golf() {
+		
+	}
+	
+	@GetMapping("SilverTour")
+	public void silver() {
+		
+	}
+	
+	
+	
+	
+	
+	
 }
