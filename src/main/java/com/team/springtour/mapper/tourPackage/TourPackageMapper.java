74b84f9ae15bab325c04fa376d21c3d9e6d1c8fb
stuicrge <90564523+stuicrge@users.noreply.github.com>
2022-06-20 15:13:02 +0900

    plustourPackage

--- src/main/java/com/team/springtour/mapper/tourPackage/TourPackageMapper.java
+++ src/main/java/com/team/springtour/mapper/tourPackage/TourPackageMapper.java
@@ -2,14 +2,21 @@ package com.team.springtour.mapper.tourPackage;
 
 import java.util.List;
 
+import com.team.springtour.domain.flight.FlightDto;
 import com.team.springtour.domain.tourPackage.TourPackageDto;
 
 public interface TourPackageMapper {
+
+	
 
 	int insertBoard(TourPackageDto tourPackage);
 
 	TourPackageDto getTourPackageByPackageName(String packageName);
 
 	List<TourPackageDto> selectTourPackageAll();
+
+	List<String> selectAuthByUserId(String userId);
+
+	List<FlightDto> selectFlightAll();
 	
 }
